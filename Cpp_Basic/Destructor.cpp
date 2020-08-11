6550274a566aec6b68cc32ab1b1e097784a1759d
Jung Minwoo <46263190+Jung-Min-woo@users.noreply.github.com>
2020-08-11 15:21:46 +0900

    Destructor

---
+++ Cpp_Basic/Destructor.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+class Person {
+private:
+	char* name;
+	int age;
+public:
+	Person(const char* myname, int myage) {
+		int len = strlen(myname) + 1;
+		name = new char[len];
+		strcpy(name, myname);
+		age = myage;
+	}
+	void ShowPersonInfo() const {
+		cout << "Name: " << this->name<<endl;
+		cout << "Age: " << this->age << endl;
+	}
+	~Person() {
+		delete[] name;
+		cout << "called destructor" << endl;
+	}
+};
+int main(void) {
+	Person man1("Lee Dong Woo", 29);
+	Person man2("Jang Dong Gun", 41);
+	man1.ShowPersonInfo();
+	man2.ShowPersonInfo();
+	return 0;
+}
