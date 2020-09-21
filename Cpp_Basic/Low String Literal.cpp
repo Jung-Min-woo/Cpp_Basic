7eacfa1708ab735fc5d87c4f64a5b5ed65bb314b
Jung Minwoo <46263190+Jung-Min-woo@users.noreply.github.com>
2020-09-21 16:37:48 +0900

    Low String Literal++

--- Cpp_Basic/Low String Literal.cpp
+++ Cpp_Basic/Low String Literal.cpp
@@ -8,9 +8,12 @@ int main(void) {
 	const char* str4 = R"(Line1
 Line2)";
 
+	const char* str5 = R"!("(Embedded)" charaters)!"; //  R"d-char-sequence(r-char-sequence)d-char-sequence"; d : delimeter
+
 	std::cout << str1 << std::endl;
 	std::cout << str2 << std::endl;
 	std::cout << str3 << std::endl;
 	std::cout << str4 << std::endl;
+	std::cout << str5 << std::endl;
 	return 0;
 }
